c5e5e964341ba50663939b68c2b12771d676721b
gnanavinayagan <31382149+gnanavinayagan@users.noreply.github.com>
2023-03-16 22:50:45 +0530

    ts

--- sorting/BubbleSort.cpp
+++ sorting/BubbleSort.cpp
@@ -4,38 +4,38 @@ using namespace std;
 
 void insertionSort(int *a, int n) {
 
-	for (int i = 1; i <= n - 1; i++) {
+  for (int i = 1; i <= n - 1; i++) {
 
-		int current = a[i];
-		int prev = i - 1;
+    int current = a[i];
+    int prev = i - 1;
 
-		while (prev >= 0 && a[prev] > current ) {
-			a[prev + 1] = a[prev];
-			prev--;
-		}
-		a[prev + 1] = current;
-	}
+    while (prev >= 0 && a[prev] > current ) {
+      a[prev + 1] = a[prev];
+      prev--;
+    }
+    a[prev + 1] = current;
+  }
 
 }
 
 void bubbleSort(int *a, int n) {
-	for (int times = 1; times <= n - 1; times++) {
+  for (int times = 1; times <= n - 1; times++) {
 
-		for (int j = 0; j <= n - 1 - times; j++) {
-			if (a[j] > a[j + 1]) {
-				swap(a[j], a[j + 1]);
-			}
+    for (int j = 0; j <= n - 1 - times; j++) {
+      if (a[j] > a[j + 1]) {
+        swap(a[j], a[j + 1]);
+      }
 
-		}
-	}
+    }
+  }
 }
 
 
 
 void printArray(int *arr, int n) {
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+  cout << endl;
 }
 
 
@@ -44,18 +44,18 @@ int main()
 {
 
 #ifndef ONLINE_JUDGE
-	freopen("output.txt", "w", stdout);
+  freopen("output.txt", "w", stdout);
 #endif
 
-	int arr[] = { -2, -3, 4, -1, 5, -12, 6, 1, 3};
-	int n = sizeof(arr) / sizeof(int);
+  int arr[] = { -2, -3, 4, -1, 5, -12, 6, 1, 3};
+  int n = sizeof(arr) / sizeof(int);
 
-	printArray(arr, n);
-	bubbleSort(arr, n);
-	// insertionSort(arr, n);
+  printArray(arr, n);
+  bubbleSort(arr, n);
+  // insertionSort(arr, n);
 
 
-	printArray(arr, n);
+  printArray(arr, n);
 
 
 
--- sorting/InsertionSort.cpp
+++ sorting/InsertionSort.cpp
@@ -10,25 +10,25 @@ using namespace std;
 
 void insertionSort(int *a, int n) {
 
-	for (int i = 1; i <= n - 1 ; i++) {
+  for (int i = 1; i <= n - 1 ; i++) {
 
-		int current = a[i];
+    int current = a[i];
 
-		int prev = i - 1;
+    int prev = i - 1;
 
-		while (prev >= 0 && a[prev] > current) {
-			a[prev + 1] = a[prev];
-			prev--;
-		}
-		a[prev + 1] = current;
+    while (prev >= 0 && a[prev] > current) {
+      a[prev + 1] = a[prev];
+      prev--;
+    }
+    a[prev + 1] = current;
 
-	}
+  }
 }
 
 void printArray(int *arr, int n) {
-	for (int i = 0; i < n; i++)
-		cout << arr[i] << " ";
-	cout << endl;
+  for (int i = 0; i < n; i++)
+    cout << arr[i] << " ";
+  cout << endl;
 }
 
 
@@ -37,17 +37,17 @@ int main()
 {
 
 #ifndef ONLINE_JUDGE
-	freopen("output.txt", "w", stdout);
+  freopen("output.txt", "w", stdout);
 #endif
 
-	int arr[] = { -2, -3, 4, -1, 5, -12, 6, 1, 3};
-	int n = sizeof(arr) / sizeof(int);
+  int arr[] = { -2, -3, 4, -1, 5, -12, 6, 1, 3};
+  int n = sizeof(arr) / sizeof(int);
 
-	printArray(arr, n);
-	insertionSort(arr, n);
+  printArray(arr, n);
+  insertionSort(arr, n);
 
 
-	printArray(arr, n);
+  printArray(arr, n);
 
 
 
